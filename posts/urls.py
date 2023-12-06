b314ef3b6d6a954e905ec9dc89552bc031ab6703
townsend75 Townsend <dtownsend@gmx.net>
2023-12-06 17:09:59 +0000

    add / to urls in post

--- posts/urls.py
+++ posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from posts import views
 
 urlpatterns = [
-    path('posts/', views.PostList.as_view()),
-    path('posts/<int:pk>/', views.PostDetail.as_view(),),
+    path('/posts/', views.PostList.as_view()),
+    path('/posts/<int:pk>/', views.PostDetail.as_view(),),
     # path('post/review', views.stars),
     ]
