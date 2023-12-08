38ba1576383d1dbcc1b572c56a3aaae50a73e3e1
townsend75 Townsend <dtownsend@gmx.net>
2023-12-08 08:58:07 +0000

    alter reviews urls to conform with others

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from reviews import views
 
 urlpatterns = [
-    path('<int:pk>/reviews', views.ReviewList.as_view(), name='reviews'),
-    path('<int:pk>/review-create', views.ReviewCreate.as_view(), name='review_create'),
+    path('/reviews/<int:pk>', views.ReviewList.as_view(), name='reviews'),
+    path('/review-create/<int:pk>', views.ReviewCreate.as_view(), name='review_create'),
     # path('<int:pk>/review/', ReviewDetail.as_view(), name='review_detail')
 ]
